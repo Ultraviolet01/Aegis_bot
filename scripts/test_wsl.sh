edb7866645e4e3203001b06303ac5f0cb5212c23
Abubakar Adeleke <ultravioletadewale@gmail.com>
2026-09-13 09:58:16 +0100

    chore: remove tests, diagnostic scripts, and project brief from git tracking

--- scripts/test_wsl.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-export PATH="/root/.avm/bin:/root/.local/share/solana/install/active_release/bin:/root/.cargo/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-cd /mnt/c/Users/USER/Downloads/Aegis
-export CARGO_TARGET_DIR="/root/target"
-anchor test --skip-build
